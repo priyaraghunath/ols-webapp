e466ab58ad0c262ef168dce63862bc7c4e00556f
Priya Raghunath <priyankabraghunath98@gmail.com>
2026-04-28 10:57:55 -0400

    entryoint file modification

--- docker-start.sh
+++ docker-start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+set -e
 
-echo "Starting entrypoint script..."
+echo "==> Injecting DB credentials into wp-config.php"
 
-# Substitute real values from ECS env vars into wp-config.php at startup
 sed -i "s|database_name_here|${WORDPRESS_DB_NAME}|g" /var/www/vhosts/localhost/html/wp-config.php
 sed -i "s|username_here|${WORDPRESS_DB_USER}|g" /var/www/vhosts/localhost/html/wp-config.php
 sed -i "s|password_here|${WORDPRESS_DB_PASSWORD}|g" /var/www/vhosts/localhost/html/wp-config.php
 sed -i "s|localhost|${WORDPRESS_DB_HOST}|g" /var/www/vhosts/localhost/html/wp-config.php
 
-echo "wp-config.php updated with database credentials"
+echo "==> wp-config.php updated:"
+grep -E "DB_NAME|DB_USER|DB_PASSWORD|DB_HOST" /var/www/vhosts/localhost/html/wp-config.php
 
-# Hand off to the original OpenLiteSpeed entrypoint
-exec /usr/local/bin/docker-entrypoint.sh "$@"
+echo "==> Starting OpenLiteSpeed"
+exec /entrypoint.sh
